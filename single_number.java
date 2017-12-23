24b4a61adfd771284ae4623c7f0caf62661ec9be
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2017-12-23 20:25:40 +0800

    Create single_number.java

---
+++ single_number.java
@@ -0,0 +1,11 @@
+package single_number;
+
+public class single_number {
+	public static void main(String[] args)
+	{
+		solution s=new solution();
+		int[] array={2,1,3,4,3,2,0,0,4};
+		s.singleNumber(array);
+	}
+
+}
